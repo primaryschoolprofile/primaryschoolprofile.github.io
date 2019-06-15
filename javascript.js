eac298ae0778025b95c8382be960afb2a3d952df
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-15 22:47:54 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function wrap(item){
 function gen_html_all(options, index){
   result = ""
   if (options[index].length > 3) {
-    result = wrap("清除")
+    result = wrap("清除").replace("checked", "")
   }
   return result
 }
