7f8091917c0faa3f72375a5d901b8ba18b50b4d7
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-15 17:33:39 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
 $(function(){
-  $(".test").load("https://primaryschoolprofile.github.io/display.txt");
+  
+  $.get("https://primaryschoolprofile.github.io/options.txt", function(data, status){
+    options = eval(data);
+    district = options[1]
+    for (x in district) {
+      $(".district").append(x)
+    }
+  });
+  
 });
