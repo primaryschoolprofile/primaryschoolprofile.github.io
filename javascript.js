c540087ee073a6e3c6618e48ab1340d386b3454e
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-15 14:46:14 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,6 @@
 $(function(){
-  var display;
-  var filter;
-  var options;
-  $.post("https://primaryschoolprofile.github.io/display.txt", function(data, status){
-    $(".test").html(data);
-  });
-  
+  //$.post("https://primaryschoolprofile.github.io/display.txt", function(data, status){
+    //$(".test").html(data);
+  //});
+  $(".test").html("test")
 });
