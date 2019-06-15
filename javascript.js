635a8bf820645aed700cecf1c9eea5662a3a7330
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-15 20:54:08 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -42,7 +42,7 @@ $(function(){
   $.get("https://primaryschoolprofile.github.io/options.txt", function(data, status){
     $(".test").html(data);
     options = eval(data);
-    console.log("options: ", 
+    console.log("options: ", options)
     eval(gen_code("地區", "district", 1));
     eval(gen_code("校網", "net", 2));
     eval(gen_code("類別", "subsidy", 3));
