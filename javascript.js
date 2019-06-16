863f7f75c6fee704481561963656c18901edb18f
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-16 13:13:47 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function clear_all(element){
   $(element).siblings().each(function(){
-    original = $(this).html().toString();
-    console.log(original.replace("<input type='checkbox' checked=''>", "<input type='checkbox'>"))
+    original = $(this).html();
+    console.log(original.replace("type='checkbox' checked", "type='checkbox'"));
     $(this).html(original.replace("<input type='checkbox' checked=''>", "<input type='checkbox'>"));
   });
 }
