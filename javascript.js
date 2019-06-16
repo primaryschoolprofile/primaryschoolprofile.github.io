24d4508491cdf327d7bb52f42034f3f2d85c2295
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-16 12:26:59 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ function wrap(item){
 function gen_html_all(options, index){
   result = ""
   if (options[index].length > 3) {
-    result = "<button type='button' class='btn btn-primary m-1 pr-2 clear'>清除</button>"
+    result = "<span class='clear'>>(清除)</span>"
   }
   return result
 }
