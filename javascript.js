e3f1bb0cc0b4334189193b7332b0733277c41c10
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-17 21:59:49 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,7 +18,7 @@ function wrap(item){
 function gen_html_all(options, index){
   result = ""
   if (options[index].length > 3){
-    result = "<span class='pr-2' onclick='select_all(this)'>(全選)</span><span class='pr-2' onclick='clear_all(this)'>(清除)</span>"
+    result = "<span class='pr-2 text-primary' onclick='select_all(this)'>全選</span><span class='pr-2 text-primary' onclick='clear_all(this)'>清除</span>"
   }
   return result
 }
