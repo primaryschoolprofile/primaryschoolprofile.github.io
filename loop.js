8597093f1cf4bc40afb1287979e629bf5c539ba9
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-18 15:06:51 +0800

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,19 @@
+var i =  0;
+
+function loop(){
+  i = i + 1;
+  array = filter[i];
+  id = array[0];
+  district = array[1];
+  net = array[2];
+  subsidy = array[3];
+  religion = array[4];
+  connection = array[5];
+  assessment = array[6];
+  if (P("district") && P("net") && P("subsidy") && P("religion") && P("connection") && P("assessment")) {
+    $(".s-" + id).removeClass("d-none");
+  } else {
+    $(".s-" + id).addClass("d-none");
+  }
+  
+  loop()
