334145f72baf865e81fcf4b6488488988c74ea96
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-18 15:49:44 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,24 +1,15 @@
-var i =  0;
-
-function loop(){
-  $.get("https://primaryschoolprofile.github.io/filter.txt", function(info, status){
-    filter = eval(info);
-    i = i + 1;
-    array = filter[i];
-    id = array[0];
-    district = array[1];
-    net = array[2];
-    subsidy = array[3];
-    religion = array[4];
-    connection = array[5];
-    assessment = array[6];
-    if (P("district") && P("net") && P("subsidy") && P("religion") && P("connection") && P("assessment")) {
-      $(".s-" + id).removeClass("d-none");
-    } else {
-      $(".s-" + id).addClass("d-none");
-    }
-    postMessage(i);
-  });
+onmessage = function(event) {
+  array = event.data;
+  id = array[0];
+  district = array[1];
+  net = array[2];
+  subsidy = array[3];
+  religion = array[4];
+  connection = array[5];
+  assessment = array[6];
+  if (P("district") && P("net") && P("subsidy") && P("religion") && P("connection") && P("assessment")) {
+    $(".s-" + id).removeClass("d-none");
+  } else {
+    $(".s-" + id).addClass("d-none");
+  }
 }
-  
-loop()
