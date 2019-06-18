afdb88a0e42759c4d2b8024692f863b68c7d6706
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-18 23:12:34 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -18,9 +18,5 @@ onmessage = function(event){
   religion = array[4];
   connection = array[5];
   assessment = array[6].toString();
-  if (P("district") || P("net") || P("subsidy") || P("religion") || P("connection") || P("assessment")) {
-    $(".s-" + id).addClass("d-none");
-  } else {
-    $(".s-" + id).removeClass("d-none");
-  }
+  postMessage([id, P("district") || P("net") || P("subsidy") || P("religion") || P("connection") || P("assessment")]);
 }
