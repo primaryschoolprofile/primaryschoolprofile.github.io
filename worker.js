d32ff8d1dca451d0bc37b0f8c279bf8120d08998
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-21 02:14:11 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -13,11 +13,11 @@ onmessage = function(event){
   assessment_chosen = data[6]
   id = array[0];
   district = array[1];
-  net = array[2];
+  net = array[2].toString();
   subsidy = array[3];
   religion = array[4];
   connection = array[5];
-  assessment = array[6];
+  assessment = array[6].toString();
   //postMessage([id, P("district") && P("net") && P("subsidy") && P("religion") && P("connection") && P("assessment")]);
   postMessage([id, P("district"), P("net"), P("subsidy"), P("religion"), P("connection"), P("assessment")])
 }
