c76220aeba60b55ba8c5f69ee8ece7ba1979d33b
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-21 20:41:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -227,13 +227,16 @@ $(function(){
           $(".profile").html("");
           //console.log("initial", $(".profile").html());
           //console.log(pass.length)
-$(".profile").append(profile(pass[0], school))
-          $(".profile").append(profile(pass[1], school))
-          for (i = 0; i < pass.length; i++) {
-            //setTimeout(function(){$(".profile").append(profile(pass[i], school))}, i*100);
-           //console.log($(".profile").html()))
+          i = 0;
+          while (true) {
+            try {
+              $(".profile").append(profile(pass[i], school));
+              i = i + 1;
+            }
+            catch(err) {
+              break;
+            }
           }
-         //console.log("final", $(".profile").html())
         }
       });
     });
