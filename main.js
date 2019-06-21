d5c15f41652fe1e77656001e10aa68c186178655
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-21 10:36:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -151,10 +151,10 @@ $(function(){
       for (i = 0; i < items_chi.length; i++) {
         temp2 = items_chi[i];
         if (all_selected(temp2) == false) {
-          temp1 = temp1 + [[temp2]];
+          temp1 = temp1 + [temp2];
         }
       }
-      console.log(temp1);      
+      console.log(typeof temp1);      
     });
   });
 
