dc9c53fa7481f2193e525d96096d6169ac4346e3
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2019-06-21 20:35:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -227,7 +227,8 @@ $(function(){
           $(".profile").html("");
           //console.log("initial", $(".profile").html());
           //console.log(pass.length)
-
+$(".profile").append(profile(pass[0], school))
+          $(".profile").append(profile(pass[0], school))
           for (i = 0; i < pass.length; i++) {
             setTimeout(function(){$(".profile").append(profile(pass[i], school))}, i*100);
            //console.log($(".profile").html()))
