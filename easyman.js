ec8bc8e9686b05e2c628eb35f87a5801b019f897
primaryschoolprofile <51843098+primaryschoolprofile@users.noreply.github.com>
2020-10-03 09:35:46 +0800

    Update easyman.js

--- easyman.js
+++ easyman.js
@@ -1,5 +1,22 @@
 index = "https://kwanyuen.beautysalon.hk";
 
+function logo(img, heading, phase_1, phase_2, phase_3, phase_4, link="") {
+  basic = `
+    <div class="py-2"><img width="120" height="120" src="` + img + `"></div>
+    <b class="text-primary">` + heading + `</b><br>`+ phase_1 + `<br>` + phase_2 + `<br>` + phase_3 + `<br>` + phase_4;
+  if (link == "") {
+    return basic
+  } else {
+    return basic + `
+      <div class="py-2">
+        <a class="btn btn-primary" href="` + link + `" role="button">
+          <span class="fas fa-search"></span> 更多
+        </a>
+      </div>
+    `
+  }
+}
+
 $(function(){
   $(".header").html(`
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -29,6 +46,123 @@ $(function(){
         </ul>
       </div>
     </nav>
+  `);
+  $(".index").html(`
+    <div class="container">
+      <div class="py-4">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="https://www.easyman.hk/wp-content/uploads/2020/01/EAS-4_Banner_Artboard-3.jpg" class="d-block w-100" alt="EAS-4_Banner_Artboard 3">
+            </div>
+            <div class="carousel-item">
+              <img src="https://www.easyman.hk/wp-content/uploads/2020/01/EAS-4_Banner_Artboard-2.jpg" class="d-block w-100" alt="Print">
+            </div>
+            <div class="carousel-item">
+              <img src="https://www.easyman.hk/wp-content/uploads/2020/01/EAS-4_Banner-01.jpg" class="d-block w-100" alt="Print">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      <div class="py-4">
+        <b class="text-warning">商管易Easy Man</b>協助初創或發展中企業成長，不同行業及階段有面對不同的需要，透過面談仔細了解經營者的情況，提供全方位的商業發展領域的落地意見；包括政府基金資助、資訊科技應用、財務管理策劃、國內東盟發展、推廣策劃、品質認証、版權商標、商業融資。「商管易」專業的團隊來自多個商業領域，如企業導師、資訊科技專家、數碼營銷專家、會計師、律師等，協助您突破事業瓶頸，商管從此好容易。
+      </div>
+      <div class="row">
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "資訊科技應用",
+          "ERP / CRM / POS",
+          "Web & Apps開發",
+          "舊系統轉換新系統",
+          "IT服務支援方案",
+          "./website"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+        <div class="col-3 py-4 text-center">` + logo(
+          "https://www.easyman.hk/wp-content/uploads/2019/11/EAS-4_Icon_Artboard-2.png",
+          "政府基金資助",
+          "科技券計劃",
+          "拓展內銷專項基金",
+          "中小企市場推廣基金",
+          "零售業科技支援計劃",
+          "./funding"
+        ) + `
+        </div>
+      </div>
+    </div>
   `);
   $(".footer").html(`
     <div class="bg-dark text-white">
